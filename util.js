d2f04caedb60bcdb14b4e55dfbd6197cc09c7934
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-08-02 10:27:33 +0300

    Created Salla api options function

---
+++ util.js
@@ -0,0 +1,16 @@
+const constants = require('./constants')
+require('dotenv').config()
+
+const generateOptions = (_path) => {
+    return options = {
+        method: constants.method,
+        url: constants.url + _path,
+        // qs: { per_page: '10' },
+        headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${process.env.SALLA_ACCESS_TOKEN}`
+        }
+    }
+}
+
+module.exports = { generateOptions }
