ad0238eab506be1cf6f2c0ec1f09fdd17334ba88
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-08-02 10:59:22 +0300

    get all the data of the current page

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const options = generateOptions('/products')
 const { requestAPI } = require('./requestAPI');
 requestAPI(options, function (err, body) {
     if (err) throw new Error(error);
-    // Gete the pages numbers of all products 
+    // Gete the total pages numbers of all products in Salla API
     global.totalPages = JSON.parse(body).pagination.totalPages;
+
+    // Go through all pages and get all products details
+    for (let current_page = 1; current_page <= global.totalPages; current_page++) {
+
+        // get all the data of the current page
+        const options = generateOptions(`/products?page=${current_page}`)
+        requestAPI(options, function (err, body) {
+            if (err) throw new Error(error);
+            const APIData = JSON.parse(body).data;
+
+
+        })
+    }
 })
