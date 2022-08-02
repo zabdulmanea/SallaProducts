6ac0118ee8b17155bd41694214b642014f7ccd0a
Zainab Abdulmanea <zabdulmani@stu.kau.edu.sa>
2022-08-02 12:10:12 +0300

    Update per_page value to 1000

--- util.js
+++ util.js
@@ -5,7 +5,7 @@ const generateOptions = (_path) => {
     return options = {
         method: constants.method,
         url: constants.url + _path,
-        // qs: { per_page: '10' },
+        // qs: { per_page: '1000' },
         headers: {
             'Content-Type': 'application/json',
             Authorization: `Bearer ${process.env.SALLA_ACCESS_TOKEN}`
